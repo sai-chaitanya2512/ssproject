4ceffdf4c5876edf223e82715dfa9c12b84fdb7b
sai-chaitanya2512 <saichaitanya5191@gmail.com>
2023-01-26 16:55:50 +0530

    changes by pk

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,60 +1,63 @@
-import React from 'react'
-import stylebanner from "../../styles/banner/Banner.module.scss"
-import Bannerfooter from './bannerfooter'
-import Navbar from './navbar'
-import Searchbar from './searchbar'
-import { Carousel } from 'antd';
+import React from "react";
+import stylebanner from "../../styles/banner/Banner.module.scss";
+import Bannerfooter from "./bannerfooter";
+import Navbar from "./navbar";
+import Searchbar from "./searchbar";
+import { Carousel } from "antd";
 
 const Banner = () => {
-    const contentStyle = {
-        height: '52.2rem',
-        color: '#fff',
-        lineHeight: '160px',
-        textAlign: 'center',
-        // background: '',
-        dots: false,
-    };
-    return (
-        <>
+  const contentStyle = {
+    height: "52.2rem",
+    color: "#fff",
+    lineHeight: "160px",
+    textAlign: "center",
+    // background: '',
+    dots: false,
+  };
+  return (
+    <>
+      <Carousel className={stylebanner.courosal} autoplay dots={false}>
+        <div>
+          <div className={stylebanner.contentdivs} style={contentStyle}>
+            <img
+              className={stylebanner.courosalimage}
+              src="https://res.cloudinary.com/dli6rliuj/image/upload/v1674125882/pexels-pixabay-356036_tuqhgp.jpg"
+            />
+          </div>
+        </div>
+        <div>
+          <div className={stylebanner.contentdivs} style={contentStyle}>
+            <img
+              className={stylebanner.courosalimage}
+              src="https://img.theculturetrip.com/1440x/smart/wp-content/uploads/2023/01/eshwar-4uedrhp2cwk-unsplash.jpg"
+            />
+          </div>
+        </div>
+        <div>
+          <div className={stylebanner.contentdivs} style={contentStyle}>
+            <img
+              className={stylebanner.courosalimage}
+              src="https://res.cloudinary.com/dqxqjfrmh/image/upload/v1674728490/1674727627320_wmtvfa.jpg"
+            />
+          </div>
+        </div>
+        <div>
+          <div className={stylebanner.contentdivs} style={contentStyle}>
+            <img
+              className={stylebanner.courosalimage}
+              src="https://res.cloudinary.com/dli6rliuj/image/upload/v1674130507/juva-transformed_nvhiwz.jpg"
+            />
+          </div>
+        </div>
+      </Carousel>
 
-            <Carousel className={stylebanner.courosal} autoplay dots={false}>
-                <div>
-                    <div className={stylebanner.contentdivs}  style={contentStyle}>
-                        <img className={stylebanner.courosalimage} src="https://res.cloudinary.com/dli6rliuj/image/upload/v1674125882/pexels-pixabay-356036_tuqhgp.jpg" />
-                    </div >
-                </div>
-                <div>
-                    <div className={stylebanner.contentdivs}  style={contentStyle}>
-                    <img className={stylebanner.courosalimage} src="https://img.theculturetrip.com/1440x/smart/wp-content/uploads/2023/01/eshwar-4uedrhp2cwk-unsplash.jpg" />
-                    </div >
-                </div>
-                <div>
-                    <div className={stylebanner.contentdivs}  style={contentStyle}>
-                    <img className={stylebanner.courosalimage} src="https://images.unsplash.com/photo-1595650317500-4305e341ebcb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80" />
-                    </div >
-                </div>
-                <div>
-                    <div className={stylebanner.contentdivs}  style={contentStyle}>
-                    <img className={stylebanner.courosalimage} src="https://res.cloudinary.com/dli6rliuj/image/upload/v1674130507/juva-transformed_nvhiwz.jpg" />
-                    </div >
-                </div>
-            </Carousel>
+      <div className={stylebanner.whole}>
+        <Navbar />
+        <Searchbar />
+        <Bannerfooter />
+      </div>
+    </>
+  );
+};
 
-
-            <div className={stylebanner.whole}>
-
-                <Navbar />
-                <Searchbar />
-                <Bannerfooter />
-
-
-            </div>
-
-
-
-        </>
-
-    )
-}
-
-export default Banner
+export default Banner;
--- src/components/footer/footerpart1.js
+++ src/components/footer/footerpart1.js
@@ -1,87 +1,126 @@
-import React from 'react'
-import style from "../../styles/footer/footerpart1.module.scss"
-import phonepic from "../../media/phonepic.png"
-import playstore from "../../media/playstore.png"
-import iphone from "../../media/iphone.png"
-import { Button } from 'antd'
+import React, { useState } from "react";
+import style from "../../styles/footer/footerpart1.module.scss";
+import phonepic from "../../media/phonepic.png";
+import playstore from "../../media/playstore.png";
+import iphone from "../../media/iphone.png";
+import { Button } from "antd";
+import axios from "axios";
 
 const Footerpart1 = () => {
-    return (
-        <div id='contact' className={style.whole}>
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
 
-            <div className={style.sider}>
+  const sendVal = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+  const send = async () => {
+    const data = await axios.post("https://app.ssprofiles.in/user/en", values);
+    console.log(data);
+  };
+  return (
+    <div id="contact" className={style.whole}>
+      <div className={style.sider}>
+        <h1>
+          <span>SS Profiles</span> on the go
+        </h1>
+        <p>
+          {" "}
+          Download our mobile application to search for any business on the go
+        </p>
+      </div>
 
-                <h1><span>SS Profiles</span> on the go</h1>
-                <p>  Download our mobile application to search for any business on the go</p>
+      <div className={style.formdiv}>
+        <h2>CONTACT US</h2>
 
+        <form>
+          <div className={style.aller}>
+            <div className={style.address}>
+              <img
+                className={style.icons}
+                src="https://res.cloudinary.com/dli6rliuj/image/upload/v1674198608/icons8-place-marker-50_b9ojoy.png"
+              />
+
+              <p>
+                4-25/5, F5 , BZone , Ramkrishnaplaza , Sujathanagar,
+                Visakhapatnam-530051
+              </p>
             </div>
+            <br />
+            <div className={style.email}>
+              <img
+                className={style.icons}
+                src="https://res.cloudinary.com/dli6rliuj/image/upload/v1674198788/website_nvfmvv.png"
+              />
 
-            <div className={style.formdiv}>
-
-                <h2>CONTACT US</h2>
-
-
-                <form>
-                    <div className={style.aller}>
-                        <div className={style.address} >
-
-
-                            <img className={style.icons} src="https://res.cloudinary.com/dli6rliuj/image/upload/v1674198608/icons8-place-marker-50_b9ojoy.png" />
-
-                            <p>4-25/5, F5 , BZone , Ramkrishnaplaza , Sujathanagar, Visakhapatnam-530051</p>
-
-                        </div>
-                        <br />
-                        <div className={style.email} >
-
-
-                            <img className={style.icons} src="https://res.cloudinary.com/dli6rliuj/image/upload/v1674198788/website_nvfmvv.png" />
-
-                            <p>www.ssprofiles.in</p>
-
-                        </div>
-                        <br />
-                        <div className={style.phonenumber} >
-
-
-                            <img className={style.icons} src="https://res.cloudinary.com/dli6rliuj/image/upload/v1674198968/phone_cnuh6s.png" />
-
-                            <div>
-                                <p>+919182351013</p>  <p>+919392252575</p>  <p>+919182632215</p>
-                            </div>
-
-                        </div>
-                    </div>
-
-                    {/* <div className={style.rightdivoutter}> */}
-                        <div className={style.rightdiv}>
-
-                            <div className={style.namesidediv}>Name :-</div>
-                            <div><input className={style.nameinp} /></div>
-                            <div className={style.phonesidediv}>Phone No. :-</div>
-                            <div><input className={style.phoneinp} /></div>
-                            <div className={style.emailsidediv}>Email :-</div>
-                            <div><input className={style.emailinp} /></div>
-                            <div className={style.feedbacksidediv}>Feedback :-</div>
-                            <div><input className={style.feedbackinp} /></div>
-                            <div></div>
-                        <button type="submit" className={style.sendbtn}>SUBMIT</button>
-                            
-
-                        </div>
-                    {/* </div> */}
-
-
-
-                </form>
-
-
+              <p>www.ssprofiles.in</p>
             </div>
+            <br />
+            <div className={style.phonenumber}>
+              <img
+                className={style.icons}
+                src="https://res.cloudinary.com/dli6rliuj/image/upload/v1674198968/phone_cnuh6s.png"
+              />
 
-            {/* <img src={phonepic} /> */}
+              <div>
+                <p>+919392252575</p> <p>+919182351013</p> <p>+919182632215</p>
+              </div>
+            </div>
+          </div>
 
-        </div>
-    )
-}
+          {/* <div className={style.rightdivoutter}> */}
+          <div className={style.rightdiv}>
+            <div className={style.namesidediv}>Name :-</div>
+            <div>
+              <input
+                className={style.nameinp}
+                name="name"
+                onChange={(e) => sendVal(e)}
+                required
+              />
+            </div>
+            <div className={style.phonesidediv}>Phone No. :-</div>
+            <div>
+              <input
+                className={style.phoneinp}
+                name="phone"
+                onChange={(e) => sendVal(e)}
+                required
+              />
+            </div>
+            <div className={style.emailsidediv}>Email :-</div>
+            <div>
+              <input
+                className={style.emailinp}
+                name="email"
+                onChange={(e) => sendVal(e)}
+                required
+              />
+            </div>
+            <div className={style.feedbacksidediv}>Feedback :-</div>
+            <div>
+              <input
+                className={style.feedbackinp}
+                name="message"
+                onChange={(e) => sendVal(e)}
+                required
+              />
+            </div>
+            <div></div>
+            <button className={style.sendbtn} onClick={send}>
+              SUBMIT
+            </button>
+          </div>
+          {/* </div> */}
+        </form>
+      </div>
 
-export default Footerpart1
+      {/* <img src={phonepic} /> */}
+    </div>
+  );
+};
+
+export default Footerpart1;
